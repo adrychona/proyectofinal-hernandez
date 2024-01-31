6674b2e4c8fed03b84c85cd7b60f95be0b18fcf9
adrychona <adrychona@gmail.com>
2024-01-31 15:35:47 -0300

    proyecto-final-v10

--- AppMascota/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-# Register your models here.
-admin.site.register(Mascota)
-admin.site.register(Vacuna)
-admin.site.register(Consulta)
-admin.site.register(Avatar)
--- README.md
+++
@@ -1,6 +0,0 @@
-# Readme
-
-Participante: Adriana Valentina Hernandez Moreno
-
-Video de App Mascofiles funcionando: https://www.awesomescreenshot.com/video/24512471?key=3ec3c55900ed2ec04825e6f05df3a8ab
-
